5a77dda00f3caad318154327640ccece57535048
JOLLY <evifere@gmail.com>
2020-04-06 10:38:58 +0200

    display people detail

--- swapi-app/src/components/PeopleDetail.vue
+++ swapi-app/src/components/PeopleDetail.vue
@@ -1,14 +1,39 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col cols="12">
-        <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200" />
-      </v-col>
-
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Welcome to swapi app</h1>
+        <h1 class="display-2 font-weight-bold mb-3">People detail</h1>
 
-        <v-data-table :headers="headers" :items="person" :items-per-page="5" class="elevation-1"></v-data-table>
+        <!--v-data-table :headers="headers" :items="person" :items-per-page="5" class="elevation-1"></v-data-table-->
+
+        <v-card class="mx-auto" max-width="344" outlined v-if="person.name.length>0">
+          <v-list-item three-line>
+            <v-list-item-content>
+              <div class="overline mb-4">
+                from {{ person.homeworld.name}}
+                <v-btn :to="'/planets/'+person.homeworld.name" icon>
+                  <v-icon>mdi-eye</v-icon>
+                </v-btn>
+              </div>
+              <v-list-item-title class="headline mb-1">{{ person.name}}</v-list-item-title>
+              <v-list-item-subtitle>Height : {{ person.height}}</v-list-item-subtitle>
+              <v-list-item-subtitle>Mass : {{ person.mass}}</v-list-item-subtitle>
+              <v-list-item-subtitle>Hair color : {{ person.hair_color}}</v-list-item-subtitle>
+              <v-list-item-subtitle>Skin color : {{ person.skin_color}}</v-list-item-subtitle>
+              <v-list-item-subtitle>Eye color : {{ person.eye_color}}</v-list-item-subtitle>
+              <v-list-item-subtitle>Gender : {{ person.gender}}</v-list-item-subtitle>
+              <v-list-item-subtitle>Birth year : {{ person.birth_year}}</v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-avatar tile size="80" color="grey">
+              <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200" />
+            </v-list-item-avatar>
+          </v-list-item>
+
+          <v-card-actions>
+            <v-btn to="/people" text>Back</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -19,31 +44,20 @@ export default {
   name: "PeopleDetail",
 
   data: () => ({
-    headers: [
-      {
-        text: "Name",
-        align: "start",
-        sortable: false,
-        value: "name"
-      },
-      { text: "Height", value: "height" },
-      { text: "Mass (g)", value: "mass" },
-      { text: "Hair color", value: "hair_color" },
-      { text: "Skin color", value: "skin_color" },
-      { text: "Birth year", value: "birth_year" }
-    ],
-    person: []
+    person: { name: "" }
   }),
 
   mounted() {
-      console.log('this.$route.params',this.$route.params)
+    console.log("this.$route.params", this.$route.params);
     this.$axios
       .post("http://localhost:8080/", {
         query:
-          '{person( name:"'+this.$route.params.name+'") {name height mass hair_color skin_color birth_year gender}}'
+          '{person( name:"' +
+          this.$route.params.name +
+          '") {name height mass hair_color skin_color birth_year gender homeworld{name}}}'
       })
       .then(response => {
-        this.person.push(response.data.data.person);
+        this.person = response.data.data.person;
       })
       .catch(error => {
         console.log(error);
