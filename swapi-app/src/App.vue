7ad0a2aa6f54ef4a37652d49f33576fd0bf06674
JOLLY <evifere@gmail.com>
2020-04-07 11:14:01 +0200

    fix

--- swapi-app/src/App.vue
+++ swapi-app/src/App.vue
@@ -22,7 +22,10 @@
         <router-link to="/starships/Millennium Falcon">Go to Starship Millennium Falcon</router-link>|
         <router-link to="/species">Go to Species</router-link>|
         <router-link to="/species/Trandoshan">Go to Species Trandoshan</router-link>|
+      </p>
+      <p>
         <router-link to="/planets">Go to Planets</router-link>|
+        <router-link to="/planets/Alderaan">Go to Planet Alderaan</router-link>|
         <router-link to="/people">Go to People</router-link>|
         <router-link to="/people/C-3PO">Go to People C-3PO</router-link>|
         <router-link to="/films">Go to Films</router-link>|
