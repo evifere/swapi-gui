4c369c4899bbe6ca09959b7212229a88747a53b4
JOLLY <evifere@gmail.com>
2020-04-09 14:18:29 +0200

    add planet list

--- swapi-reactapp/src/pages/PlanetList.js
+++ swapi-reactapp/src/pages/PlanetList.js
@@ -1,9 +1,87 @@
 import React from "react";
+import axios from 'axios';
+import { withStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-export default function PlanetList() {
-    return (
-      <div>
-        <h1>Planets</h1>
-      </div>
-    );
-  }
+
+const useStyles = theme => ({
+    table: {
+        minWidth: 650,
+    },
+});
+
+class PlanetList extends React.Component {
+
+    state = {
+        planets: []
+    }
+
+
+    componentDidMount() {
+
+        axios
+            .post("http://localhost:8080/", {
+                query:
+                    "{planets {name rotation_period orbital_period diameter climate terrain}}"
+            })
+            .then(response => {
+                const planets = response.data.data.planets;
+                this.setState({ planets });
+
+            })
+            .catch(error => {
+                console.log(error);
+                this.errored = true;
+            })
+            .finally(() => (this.loading = false));
+    }
+
+    render() {
+        const { classes } = this.props;
+        const rows = this.state.planets;
+
+        return (
+            <Container maxWidth="md">
+                <h1>Planets</h1>
+                <TableContainer component={Paper}>
+                    <Table className={classes.table} size="small" aria-label="a dense table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell align="right">Rotation period</TableCell>
+                                <TableCell align="right">Orbital period</TableCell>
+                                <TableCell align="right">Diameter</TableCell>
+                                <TableCell align="right">Climate</TableCell>
+                                <TableCell align="right">Terrain</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map((row) => (
+                                <TableRow key={row.name}>
+                                    <TableCell component="th" scope="row">
+                                        {row.name}
+                                    </TableCell>
+                                    <TableCell align="right">{row.name}</TableCell>
+                                    <TableCell align="right">{row.rotation_period}</TableCell>
+                                    <TableCell align="right">{row.orbital_period}</TableCell>
+                                    <TableCell align="right">{row.climate}</TableCell>
+                                    <TableCell align="right">{row.terrain}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Container>
+
+        )
+    }
+}
+
+export default withStyles(useStyles)(PlanetList)
--- swapi-reactapp/src/routes.js
+++ swapi-reactapp/src/routes.js
@@ -1,15 +1,20 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
 } from "react-router-dom";
 
 import Home from './pages/Home';
 import About from './pages/About';
 import Dashboard from './pages/Dashboard';
 import PlanetList from './pages/PlanetList';
+import AppBar from '@material-ui/core/AppBar';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
 // This site has 3 pages, all of which are rendered
 // dynamically in the browser (not server rendered).
@@ -19,50 +24,67 @@ import PlanetList from './pages/PlanetList';
 // through the site. This preserves the browser history,
 // making sure things like the back button and bookmarks
 // work properly.
+const useStyles = makeStyles((theme) => ({
+    icon: {
+        marginRight: theme.spacing(2),
+    },
+}))
 
 export default function AppRouter() {
-  return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/planets">Planets</Link>
-          </li>
-        </ul>
+    const classes = useStyles();
 
-        <hr />
+    return (
+        <Router>
+            <AppBar position="relative">
+                <Toolbar>
+                    <CameraIcon className={classes.icon} />
+                    <Typography variant="h6" color="inherit" noWrap>
+                        Swapi -app
+          </Typography>
+                </Toolbar>
 
-        {/*
+            </AppBar>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard">Dashboard</Link>
+                    </li>
+                    <li>
+                        <Link to="/planets">Planets</Link>
+                    </li>
+                </ul>
+            </div>
+            <main>
+                <hr />
+
+                {/*
           A <Switch> looks through all its children <Route>
           elements and renders the first one whose path
           matches the current URL. Use a <Switch> any time
           you have multiple routes, but you want only one
           of them to render at a time
         */}
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/planets">
-            <PlanetList />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                    <Route path="/planets">
+                        <PlanetList />
+                    </Route>
+                </Switch>
+            </main>
+        </Router>
+    );
 }
