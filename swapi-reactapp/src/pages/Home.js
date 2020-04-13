1c442eb465f7cbf34e2c763d4e28a7fbebc7b8fd
JOLLY <evifere@gmail.com>
2020-04-13 09:49:40 +0200

    define Home as a class

--- swapi-reactapp/src/pages/Home.js
+++ swapi-reactapp/src/pages/Home.js
@@ -1,32 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Home() {
-    return (
-      <div>
-        <h2>Home</h2>
-        <div>
-                <ul>
-                    <li>
-                        <Link to="/planets">Planets</Link>
-                    </li>
-                    <li>
-                        <Link to="/people">People</Link>
-                    </li>
-                    <li>
-                        <Link to="/species">Species</Link>
-                    </li>
-                    <li>
-                        <Link to="/starships">Starships</Link>
-                    </li>
-                    <li>
-                        <Link to="/vehicles">Vehicles</Link>
-                    </li>
-                    <li>
-                        <Link to="/films">Films</Link>
-                    </li>
-                </ul>
-        </div>
-      </div>
-    );
-  }
+
+class Home extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h2>Home</h2>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/planets">Planets</Link>
+                        </li>
+                        <li>
+                            <Link to="/people">People</Link>
+                        </li>
+                        <li>
+                            <Link to="/species">Species</Link>
+                        </li>
+                        <li>
+                            <Link to="/starships">Starships</Link>
+                        </li>
+                        <li>
+                            <Link to="/vehicles">Vehicles</Link>
+                        </li>
+                        <li>
+                            <Link to="/films">Films</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default Home
