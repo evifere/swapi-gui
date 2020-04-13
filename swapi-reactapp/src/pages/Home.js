fcd328255961b759bf9a63a6f716800ce79540bb
JOLLY <evifere@gmail.com>
2020-04-13 11:18:05 +0200

    cssing home

--- swapi-reactapp/src/pages/Home.js
+++ swapi-reactapp/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { withStyles } from '@material-ui/core/styles';
 import ButtonBaseList from "../components/ButtonBaseList"
 
 const images = [
@@ -6,45 +7,53 @@ const images = [
         url: '/assets/people_category.png',
         title: 'Characters',
         width: '30%',
-        to:'/people'
+        to: '/people'
     },
     {
         url: '/assets/planet_category.png',
         title: 'Planets',
         width: '30%',
-        to:'/planets'
+        to: '/planets'
     },
     {
         url: '/assets/species_category.png',
         title: 'Species',
         width: '30%',
-        to:'/species'
+        to: '/species'
     },
     {
         url: '/assets/starship_category.png',
         title: 'Starships',
         width: '30%',
-        to:'/starships'
+        to: '/starships'
     },
     {
         url: '/assets/film_category.png',
         title: 'Films',
         width: '30%',
-        to:'/films'
+        to: '/films'
     },
     {
         url: '/assets/vehicle_category.png',
         title: 'Vehicles',
         width: '30%',
-        to:'/vehicles'
+        to: '/vehicles'
     },
 ];
+
+const useStyles = ((theme) => ({
+    root: {
+        marginTop: '5rem',
+    }
+}));
 
 class Home extends React.Component {
 
     render() {
+        const { classes } = this.props;
+
         return (
-            <div>
+            <div className={classes.root}>
                 <ButtonBaseList images={images} />
             </div>
         );
@@ -52,4 +61,4 @@ class Home extends React.Component {
 
 }
 
-export default Home
+export default withStyles(useStyles)(Home);
